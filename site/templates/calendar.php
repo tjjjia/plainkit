31ebfdc1d884b6f65af5f3ac522607e613928a61
rubenverkuylen <info@rubenverkuylen.nl>
2024-03-06 12:05:11 +0100

    styling update

--- site/templates/calendar.php
+++ site/templates/calendar.php
@@ -3,9 +3,7 @@
 <div id="index" class="main-container">
     <ul class="events" data-page="<?= $pagination->nextPage() ?>" id="listed">
     <?php foreach($events as $event): ?>
-        <li>
-            <?= snippet('listitem', ['event' => $event]); ?>
-        </li>
+        <?= snippet('listitem', ['event' => $event]); ?>
     <?php endforeach ?>
 
     </ul>
