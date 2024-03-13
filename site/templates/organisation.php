cff349baddd0965c8365ccdf9cc7f344d69c22b0
rubenverkuylen <info@rubenverkuylen.nl>
2024-03-13 21:54:26 +0100

    organisation static page TEMP

--- site/templates/organisation.php
+++ site/templates/organisation.php
@@ -1,8 +1,14 @@
 <?php snippet('header') ?>
 
 <div id="index" class="main-container">
+    <ul class="events" data-page="1" id="listed">
+            <?php foreach($site->page('network')->children() as $event): ?>
+                <?= snippet('listitem', ['event' => $event]); ?>
+            <?php endforeach ?>
+    </ul>
 </div>
 <div id="render" class="main-container">
+    <?php snippet('organisation', ['organisation' => $page]) ?>
 </div>
 
 <?php snippet('footer') ?>
