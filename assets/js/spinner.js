14e5117cd8e066f42131a92351cd2a03f5a5d258
rubenverkuylen <info@rubenverkuylen.nl>
2024-03-13 12:31:39 +0100

    spinner

---
+++ assets/js/spinner.js
@@ -0,0 +1,11 @@
+const renderSpinner = function (parentEl) {
+  const markup = `
+    <div class="spinner">
+      <svg>
+          <use href="assets/img/icons.svg#icon-loader"></use>
+      </svg>
+    </div>
+    `;
+  parentEl.innerHTML = "";
+  parentEl.insertAdjacentHTML("afterbegin", markup);
+};
